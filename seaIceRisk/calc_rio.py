37a19bb41785d2a8776b6829e2e0b1d70e630853
Stephen Price <sprice@lanl.gov>
2026-06-19 11:42:50 -0600

    minor edits to existing files; addition of two new workbooks for plotting maps of RIO + route and thickness + route

--- seaIceRisk/calc_rio.py
+++ seaIceRisk/calc_rio.py
@@ -1,9 +1,23 @@
 from polaris_rio import polar_rio
 
+# case 1: 
+# mean thickness = 0.5 m
+# concentration = 0.5
+# actual thickness in areas where there are ice = 0.5 m / 0.5 = 1.0 m
+# thickness used to calculate RIV = 1.0 m
+
+# case 2: 
+# mean thickness = 0.5 m
+# concentration = 0.5
+# thickness used to calculate RIV = 0.5 m
+
+# Which one is correct in the context of sea ice daily stats from mpas-si?
+# They give quite diff. RIO values (For NIS: case 1 RIO=-10; case 2 RIO=0)
+
 rio, ice_type, level = polar_rio(
-    vessel_ice_class="PC3",
-    ice_thickness_m=0.8,
-    concentration_tenths=10
+    vessel_ice_class="NIS",
+    ice_thickness_m=0.5,
+    concentration_tenths=5
 )
 
 print(f"RIO = {rio}")
